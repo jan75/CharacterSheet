d644901e94470805295673c0acfa63c7120e50c6
jan75 <janackermann@bluewin.ch>
2015-12-16 18:29:51 +0100

    UPdated ParserOperations

--- src/ParserOperations.java
+++ src/ParserOperations.java
@@ -1,8 +1,26 @@
-public class ParserOperations {
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+
+class ParserOperations {
 	
-	static void main(String[] args) {
-		System.out.println("Hello World!");
+    public static void main(String[] args) {        
+        parseString("data/spells.xml");
+    }
+    
+    public static String parseString(String path) {
+		File file = new File(path);
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document document = db.parse(file);
+		} catch (Exception e) {
+			//
+		}
+		//
+		System.out.println(path);
+		//
+		return path;
 	}
-	
 	
 }
