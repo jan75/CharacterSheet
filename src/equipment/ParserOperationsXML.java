c1d370373273c3ecc219d0cb558ab71e19bac3fd
jan75 <janackermann@bluewin.ch>
2015-12-17 21:13:30 +0100

    ParserOperationsXML updated, it now parses weapons.xml and creates a Map out of the contents

--- src/equipment/ParserOperationsXML.java
+++ src/equipment/ParserOperationsXML.java
@@ -3,26 +3,62 @@ package equipment;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class ParserOperationsXML {
 	
     public static void main(String[] args) {        
-        parseString("data/weapons.xml");
+        Map myMap = parseWeapons("src/data/weapons.xml");
+		//
     }
 
-    public static String parseString(String path) {		
-		Weapon weapon = new Weapon("bla1","bla2","bla3","bla4","bla5","bla6");
-		weapon.print();
-		/*
+	/**
+	 * The Method "parseWeapons" parses an input file (XML, specified via parameter "path") and fills its contents into a HashMap (String, Weapon). A Map is returned.
+	 * @param path
+	 * @return Map
+	 */
+    public static Map parseWeapons(String path) {
+		//Weapon weapon = new Weapon("Greatsword","10 lbs","5S","1d6 piercing","bla5","bla6");
+		// weapon.print();
+		Map<String, Weapon> weaponMap = new HashMap<>();
+		//
 		File file = new File(path);
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		try {
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document document = db.parse(file);
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document document = dBuilder.parse(path);
+			//
+			document.getDocumentElement().normalize(); // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+			//
+			//System.out.println("Root element: " + document.getDocumentElement().getNodeName());
+			//System.out.println("----------------------------");
+			//
+			NodeList nList = document.getElementsByTagName("name");
+			//
+			for(int i = 0; i < nList.getLength(); i++) {
+				Node nNode = nList.item(i);
+				//
+				if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					//
+					String tmpWeapon = eElement.getAttribute("weaponname");
+					String tmpPrice = eElement.getElementsByTagName("price").item(0).getTextContent();
+					String tmpDamage = eElement.getElementsByTagName("damage").item(0).getTextContent();
+					String tmpWeight = eElement.getElementsByTagName("weight").item(0).getTextContent();
+					String tmpGroup = eElement.getElementsByTagName("group").item(0).getTextContent();
+
+					Weapon tmpWeaponObject = new Weapon(tmpWeapon, tmpPrice, tmpDamage, tmpWeight, tmpGroup);
+					//tmpWeaponObject.print();
+					//
+					weaponMap.put(tmpWeapon, tmpWeaponObject);
+					//
+				}
+			}
 		} catch (Exception e) {
-			System.out.println("File " + path + " not found!");
+			e.printStackTrace();
 		}
-		*/
-		return path;
+		//
+		return weaponMap;
 	}
 }
--- src/equipment/Weapon.java
+++ src/equipment/Weapon.java
@@ -1,18 +1,19 @@
 package equipment;
 
 class Weapon extends Equipment {
-	String weaponName, weaponWeight, weaponPrice, weaponProperty, weaponGroup, weaponModifier;
-	
-	Weapon(String weaponName, String weaponWeight, String weaponPrice, String weaponProperty, String weaponGroup, String weaponModifier) {
+	String weaponName, weaponWeight, weaponPrice, weaponGroup, weaponDamage;
+	// also available: weaponProperty, weaponModifier
+	//
+	Weapon(String weaponName, String weaponDamage, String weaponWeight, String weaponPrice, String weaponGroup) {
 		this.weaponName = weaponName;
+		this.weaponDamage = weaponDamage;
 		this.weaponWeight = weaponWeight;
 		this.weaponPrice = weaponPrice;
-		this.weaponProperty = weaponProperty;
 		this.weaponGroup = weaponGroup;
-		this.weaponModifier = weaponModifier;
 	}
 	
 	void print() {
-		System.out.println("Weapons:\n Name: " + weaponName + "\nPrice " + weaponPrice);
+		System.out.println("Weapon:\t\t" + weaponName + "\nDamage:\t\t" + weaponDamage + "\nWeight:\t\t" + weaponWeight + "\nPrice:\t\t" + weaponPrice + "\nGroup:\t\t" + weaponGroup);
+		System.out.println("\n------------------------------------\n");
 	}
 }
